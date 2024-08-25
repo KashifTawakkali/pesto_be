c0d73c70a88e959a49de20b23d05471805b11ce4
kashif <kashu@kashif>
2024-08-25 02:25:06 -0700

    change in task route

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { createTask } = require('../controllers/taskController');
+const { updateTaskStatus } = require('../controllers/updateTaskController');
+const { getAllTasks } = require('../controllers/getTaskController');
 const auth = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Route to create a new task
 router.post('/create', auth, createTask);
+
+// Route to update task status
+router.post('/update-status', auth, updateTaskStatus);
+
+// Route to get all tasks
+router.get('/all', auth, getAllTasks);
 
 module.exports = router;
